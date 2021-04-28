6580f5b5d97fc045bdaf0553261ebacd54d18bf3
UmutAslan-cloud <umutchinax@gmail.com>
2021-04-28 22:16:42 +0200

    React Router Used

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@
 import './App.css';
 import FormHandling from "./components/FormHandling"
 import List from "./components/List"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-   <FormHandling data-testid="form-area"/>
-   <List data-testid="list-area"/>
+    
+    <Router>
+    <div>
+      <nav>
+            <Link to="/FormHandling">Form</Link>
+            <Link to="/List">List</Link>
+      </nav>
+
+      <Switch>
+        <Route path="/FormHandling">
+          <FormHandling data-testid="form-area" />
+        </Route>
+        <Route path="/List">
+          <List data-testid="list-area" />
+        </Route>
+        
+      </Switch>
     </div>
+  </Router>
   );
 }
+
 
 export default App;
--- src/components/FormHandling.js
+++ src/components/FormHandling.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useState } from 'react';
+import './Form.css';
 
 
 const formReducer = (state, event) => {
